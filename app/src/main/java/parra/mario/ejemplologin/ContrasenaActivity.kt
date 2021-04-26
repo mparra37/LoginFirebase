64a77ea2acc6575a266c4e2cdab19568652fc866
mparra37 <marioparra375@gmail.com>
2021-04-26 06:03:32 -0700

    Add layouts

---
+++ app/src/main/java/parra/mario/ejemplologin/ContrasenaActivity.kt
@@ -0,0 +1,11 @@
+package parra.mario.ejemplologin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContrasenaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contrasena)
+    }
+}
---
+++ app/src/main/java/parra/mario/ejemplologin/RegistroActivity.kt
@@ -0,0 +1,11 @@
+package parra.mario.ejemplologin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+    }
+}
